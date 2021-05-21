f5ea9f53f35a65cd34c8cbd20ef957241ae67505
trungkien4723 <ntrungkien4723@gmail.com>
2021-05-21 16:19:31 +0700

    su dung giao dien login auth

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace'=>'Manage',],function(){
+Route::group(['namespace'=>'Manage', 'middleware'=>['auth',]],function(){
 
     Route::get('/admin', function (){
         return view('manage.layout.app');
@@ -31,3 +31,4 @@ Route::group(['namespace'=>'Client'],function(){
 });
 
 Auth::routes();
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
