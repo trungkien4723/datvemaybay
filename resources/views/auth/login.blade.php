24dd096b3ee58668a7ce686622b60cbfe146bc61
trungkien4723 <nTrungkien4723@gmail.com>
2021-05-22 22:43:35 +0700

    tao login yeu cau neu vao trang admin thi phai la admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Đăng nhập tài khoản') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Giữ tôi đăng nhập') }}
                                     </label>
                                 </div>
                             </div>
@@ -54,14 +54,18 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Đăng nhập') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Quên mật khẩu?') }}
                                     </a>
                                 @endif
+                                    </br>
+                                    <a class="btn btn-link" href="{{ route('register') }}">
+                                        {{ __('Chưa có tài khoản?') }}
+                                    </a>
                             </div>
                         </div>
                     </form>
