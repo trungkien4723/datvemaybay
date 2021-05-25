b9db9678a8225018b92bf3ca7ff26c55d74027ee
trungkien4723 <nTrungkien4723@gmail.com>
2021-05-25 20:32:47 +0700

    sua loi register va sua redirect sau register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -99,25 +99,6 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Ảnh đại diện') }}</label>
-
-                            <div class="col-md-6">
-                                <input name="image" type="file" id="image_input"/>
-
-                                <div class="col-md-6 col-lg-6 col-sm 12" id="image_preview_container">
-                                    <img src="{{asset('/images/user/Sample_User_Icon.png')}}" alt="..." width="100px" height="100px"
-                                        style="max-height: 100%;max-width: 100%" class="image-show student-image"  id="image_preview">
-                                </div>
-
-                                @error('image')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mật khẩu') }}</label>
 
                             <div class="col-md-6">
