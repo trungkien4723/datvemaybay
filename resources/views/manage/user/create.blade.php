6eee7898000eb316117033ca5e224d961a082a37
trungkien4723 <nTrungkien4723@gmail.com>
2021-05-30 14:38:56 +0700

    chinh input image chi chap nhan hinh anh

--- resources/views/manage/user/create.blade.php
+++ resources/views/manage/user/create.blade.php
@@ -25,7 +25,7 @@
                             <label for="image" class="col-md-2 col-form-label text-md-right">{{ __('Ảnh đại diện') }}</label>
 
                             <div class="col-md-4">
-                                <input  type="file" class="image_input form-control @error('image') is-invalid @enderror" name="image">
+                                <input  type="file" class="image_input form-control @error('image') is-invalid @enderror" name="image" accept="image/*">
                                 <image class="image_preview" src="{{asset('images/user/Sample_User_Icon.png')}}" name="preview-image" alt="..." style="margin-top:1rem; width:180px; height:180px;"/>
                                 <br><a class="image_input_clear btn btn-danger disabled">Xóa ảnh</a>
                                 @error('name')
